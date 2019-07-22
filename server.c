7fe082024f04e8c69291f1ac06b2c9c66733f868
Federico Matteoni <federicomatteoni@live.it>
2019-07-22 16:32:58 +0200

    SIGUSR1 scaffold

--- server.c
+++ server.c
@@ -8,21 +8,39 @@
 #include <sys/un.h>
 
 #define SOCKETNAME "./objectstore_530257"
+
 #define UNIX_PATH_MAX 108
 #define CHECK_RETVAL(value, str) \
 		if (value != 0) { \
 				perror(str); \
 		}
 
+int clientConnessi, oggettiMemorizzati, storeTotalSize;
+
 struct sockaddr_un skta;
+struct sigaction s;
 int skt;
+
+static void gest_SIGUSR1(int signum) {
+	//TODO: info server in output
+}
 
 int setupserver() {
 	int retval;
+	memset(&s, 0, sizeof(s));
+	s.sa_handler = gest_SIGUSR1;
+	retval = sigaction(SIGUSR1, &s, NULL);
+	if (retval != 0) return retval;
+	
 	strncpy(skta.sun_path, SOCKETNAME, UNIX_PATH_MAX);
 	skta.sun_family = AF_UNIX;
 	skt = socket(AF_UNIX, SOCK_STREAM, 0);
 	retval = bind(skt, (struct sockaddr *)&skta, sizeof(skta));
+	if (retval != 0) return retval;
+
+	clientConnessi = 0;
+	oggettiMemorizzati = 0;
+	storeTotalSize = 0;
 
 	return retval;
 }
