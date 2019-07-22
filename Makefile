5b2913ac144e424124ff3a73ffeddb5936d2b0d0
Federico Matteoni <federicomatteoni@live.it>
2019-07-22 16:22:11 +0200

    Server setup

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ all :
 	$(CC) $(CFLAGS) libclient.c -c -o libclient.o
 	ar rvs libclient.a libclient.o
 	$(CC) $(CFLAGS) client.c $(LDFLAGS) -lclient -o client
+	$(CC) $(CFLAGS) server.c $(LDFLAGS) -o server
 
 clean :
 	rm -f filetolist libList.a libList.o
--- server.c
+++ server.c
@@ -1,8 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #define SOCKETNAME "./objectstore_530257"
+#define UNIX_PATH_MAX 108
+#define CHECK_RETVAL(value, str) \
+		if (value != 0) { \
+				perror(str); \
+		}
+
+struct sockaddr_un skta;
+int skt;
+
+int setupserver() {
+	int retval;
+	strncpy(skta.sun_path, SOCKETNAME, UNIX_PATH_MAX);
+	skta.sun_family = AF_UNIX;
+	skt = socket(AF_UNIX, SOCK_STREAM, 0);
+	retval = bind(skt, (struct sockaddr *)&skta, sizeof(skta));
+
+	return retval;
+}
 
 int main (int argc, char *argv[]) {
-	
+	int retval;
+
+	retval = setupserver();
+	CHECK_RETVAL(retval, "Server setup");
+
+	return 0;
 }
