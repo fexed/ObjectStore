45a9e23777c4d535cd05969a47f676f573a9f93c
Federico Matteoni <federicomatteoni@live.it>
2019-07-22 15:56:49 +0200

    socket name

--- libclient.h
+++ libclient.h
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #define true 0
 #define false 1
+#define SOCKETNAME "./objectstore_530257"
 
 int os_connect(char *name); //Connette con username name. true se OK, false altrimenti
 
---
+++ server.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SOCKETNAME "./objectstore_530257"
+
+int main (int argc, char *argv[]) {
+	
+}
