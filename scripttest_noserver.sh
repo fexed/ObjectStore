5a538929f61e0246d33de4ad5dcf896862a9d890
Federico Matteoni <federicomatteoni@live.it>
2019-07-28 14:53:22 +0200

    No server test script

---
+++ scripttest_noserver.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#./server &
+
+echo "Inizio test"
+
+echo "" > testout.log
+
+echo "STORE"
+
+for i in {1..50}
+do
+	./client usr_$i 1 >> $i.log &
+	pids[$i]=$!
+done
+
+for pid in ${pids[*]}; do
+	wait $pid
+done
+
+echo "RETRIEVE"
+
+for i in {1..29}
+do
+	./client usr_$i 2 >> $i.log &
+	pids[$i]=$!
+done
+
+echo "DELETE"
+
+for i in {30..50}
+do
+	./client usr_$i 3 >> $i.log &
+	pids[$i]=$!
+done
+
+for pid in ${pids[*]}; do
+	wait $pid
+done
+
+for i in {1..50}
+do
+	cat $i.log >> testout.log
+	rm $i.log
+done
