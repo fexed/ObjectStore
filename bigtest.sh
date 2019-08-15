f7821f3cb71d1c85b418cd4b4b5b1cd73ff766d4
Federico Matteoni <federicomatteoni@live.it>
2019-08-15 15:38:29 +0200

    Different type of big test

--- bigtest.sh
+++ bigtest.sh
@@ -1,7 +1,51 @@
-for i in {1..10}
+#!/bin/bash
+./server &
+
+echo ""
+echo "**Inizio BIG test**"
+
+echo "" > testout.log
+
+echo "STORE"
+
+for i in {1..1000}
 do
-	make -i clean > /dev/null
-	make all > /dev/null 2>&1
-	make test
-	#read -p "Premere invio per continuare"
+	./client usr_$i 1 >> $i.log &
+	pids="$pids $!"
 done
+
+echo "... attendo"
+
+wait $pids
+
+pids=""
+
+echo "RETRIEVE"
+
+for i in {1..30}
+do
+	./client usr_$i 2 >> $i.log &
+	pids="$pids $!"
+done
+
+echo "DELETE"
+
+for i in {31..50}
+do
+	./client usr_$i 3 >> $i.log &
+	pids="$pids $!"
+done
+
+echo "... attendo"
+
+wait $pids
+
+for i in {1..50}
+do
+	cat $i.log >> testout.log
+	rm $i.log
+done
+echo "*******************"
+echo ""
+
+./testsum.sh
